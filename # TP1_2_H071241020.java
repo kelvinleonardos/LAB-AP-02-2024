1d654ad056745fcf7916908d640991f13bb4dbcc
Agung4513 <alloagung@gmail.com>
2024-09-09 23:20:01 +0800

    Update # TP1_2_H071241020.java

--- # TP1_2_H071241020.java
+++ # TP1_2_H071241020.java
@@ -5,5 +5,5 @@ kalimat = input("Masukkan kalimat: ")
 # Gunakan operator 'in' untuk memeriksa keberadaan karakter dalam kalimat
 pesan = ("Karakter tidak ditemukan dalam Kalimat", "Karakter merupakan bagian dari Kalimat")[karakter in kalimat]
 
-# Tampilkan pesan
+# Munculkan pesan
 print(pesan)
