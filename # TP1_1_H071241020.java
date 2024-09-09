5e7eea116c90cd3018467b177146e3b89ca92fb8
Agung4513 <alloagung@gmail.com>
2024-09-09 23:16:00 +0800

    Update # TP1_1_H071241020.java

--- # TP1_1_H071241020.java
+++ # TP1_1_H071241020.java
@@ -1,9 +1,9 @@
-# Input harga saham kemarin
-harga_kemarin = float(input("Masukkan harga saham kemarin: "))
-harga_hari_ini = float(105.0)  # Harga saham hari ini
+# Input nilai saham kemarin
+nilai_kemarin = float(input("Masukkan nilai saham kemarin: "))
+nilai_hari_ini = float(105.0)  # Nilai saham hari ini
 
 # Hitung perubahan persentase
-perubahan_persen = ((harga_hari_ini - harga_kemarin) / harga_kemarin) * 100
+perubahan_persen = ((nilai_hari_ini - nilai_kemarin) / nilai_kemarin) * 100
 
 # Tentukan rekomendasi berdasarkan perubahan persentase
 rekomendasi = ["Jual", "Tahan", "Beli"][(perubahan_persen > -3) + (perubahan_persen > 5)]
