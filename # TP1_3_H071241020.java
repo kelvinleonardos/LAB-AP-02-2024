2aaa2c72961be6d3ec352611def758a07c55e618
Agung4513 <alloagung@gmail.com>
2024-09-09 23:20:33 +0800

    Update # TP1_3_H071241020.java

--- # TP1_3_H071241020.java
+++ # TP1_3_H071241020.java
@@ -6,7 +6,7 @@ jam = jumlah_detik // 3600
 menit = (jumlah_detik % 3600) // 60
 detik = jumlah_detik % 60
 
-# Format waktu dalam jam:menit:detik
+# Format waktu dengan jam:menit:detik
 waktu = f"{jam:02}:{menit:02}:{detik:02}"
 
 # Tampilkan hasil
